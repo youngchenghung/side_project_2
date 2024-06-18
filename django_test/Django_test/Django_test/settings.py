8610323269a86cace569c19d783534037f2a8150
leo <leo7412369@gmail.com>
2024-06-19 00:10:49 +0800

    update 0619

--- django_test/Django_test/Django_test/settings.py
+++ django_test/Django_test/Django_test/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-StATICFILED_DIRS = [str(BASE_DIR) + "/static/img"]
+StATICFILED_DIRS = [str(BASE_DIR) + "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
